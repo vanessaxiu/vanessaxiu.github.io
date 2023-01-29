da14dbdfd80f1d960bf7e4d32b7d3a123f83a919
vanessaxiu <77381325+vanessaxiu@users.noreply.github.com>
2023-01-29 11:12:56 -0500

    Update test.js

--- javascripts/test.js
+++ javascripts/test.js
@@ -1,31 +1,67 @@
 
 var quiz = [
     {
-        "question": "what is the gender you were assigned at birth",
-        "choices": ["male", "female"],
-        "w_i": [48.86, 21.14]
-    }, {
-        "question": "in the last 12 months, have you had sex with a male",
-        "choices": ["yes", "choice2", "choice3", "choice4"],
-        "w_i": [1, 2, 3, 4]
-    }, {
-        "question": "Question03",
-        "choices": ["choice1", "choice2", "choice3", "choice4"],
-        "w_i": [1, 2, 3, 4]
-    }, {
-        "question": "Question04",
-        "choices": ["choice1", "choice2", "choice3", "choice4"],
-        "w_i": [1, 2, 3, 4]
-    }, {
-        "question": "Question05",
-        "choices": ["choice1", "choice2", "choice3", "choice4"],
-        "w_i": [1, 2, 3, 4]
-    }
+    "question": "what is the gender you were assigned at birth",
+    "choices":[ "male", "female"],
+    "w_i": [2, 1]
+    }, 
+    {"question": "In the last 12 months, have you had sex with a male",
+    "choices":[ "Yes", "No", "Unknown"],
+    "w_i": [4, 1, 3]
+    }, 
+    {
+    "question": "Did you use condoms with male partners in the past 12 months",
+    "choices":[ "Always", "Never", "Usually (>50%) or sometimes", "Not applicable (no vag/anal sex)" ],
+    "w_i": [1, 3, 2, 0]
+    }, 
+    {
+    "question": "In the last 12 months, have you had sex with a female",
+    "choices":[ "Yes", "No", "Unknown"],
+    "w_i": [2, 1, 0]
+    }, 
+    {
+    "question": "Did you use condoms with female partners in the past 12 months",
+    "choices":[ "Always", "Never", "Usually (>50%) or Sometimes", "Not applicable (no vag/anal sex)", "Unknown or Declined"],
+    "w_i": [0, 3, 1, 0, 1]
+    }, 
+    {
+    "question": "When was the last time you injected drugs",
+    "choices":[ "Less than 12 months ago", "Less than 3 months ago", "More than 12 months ago", "Never injected"],
+    "w_i": [1, 2, 0, 0]
+    }, 
+    {
+    "question": "Did you have Hepatitis B infection in the past",
+    "choices":[ "Yes", "No"],
+    "w_i": [2, 1]
+    }, 
+    {
+    "question": "Did you have contact with chlamydia",
+    "choices":[ "Yes", "No"],
+    "w_i": [2, 1]
+    }, 
+    {
+    "question": "Did you have other past STI infections",
+    "choices":[ "Yes", "No"],
+    "w_i": [2, 1]
+    }, 
+    {
+    "question": "Did you have gonorrhea infection in the past",
+    "choices":[ "Yes", "No"],
+    "w_i": [2, 1]
+    },
+    {"question": "what is your immigration status",
+    "choices": ["Canadian citizen, Quebec residents", "Canadian citizen, non-Quebec residents", "foreigner"],
+    "w_i": [0, 0,0]
+    },
+    {
+    "question": "what is your annual income level",
+    "choices": ["0-10000CAD", "10001-50000CAD", ">50000CAD"],
+    "w_i": [0,0,0]
+    },
 ];
 
 var i_q = 0,
     w_t = 0;
-
 function $(id) {
     return document.getElementById(id);
 }
@@ -70,10 +106,10 @@ function check() {
 
 function grade() {
     document.getElementById('submitBtn').disabled="true";
-    content.innerHTML = "<h2>Thank you for completing the test!</h2>" + "<h2>Below are your risk scale and suggested treatment options:</h2>"
-    if (w_t <10){
+    content.innerHTML = "<h1>Thank you for completing the test!</h1>" + "<h1>Below are your risk scale and suggested prevention options:</h1>" + "<h1><a href=\"https://fqsida.org/en/member-organizations/?lang=en\"><span>NGO prevention resources</span></a></h1>";
+    if (w_t <11){
     content.innerHTML += "<img src=\"images/test-low.jpeg\">";}
-    else if(w_t<20){content.innerHTML+="<img src=\"images/test-medium.jpeg\">";}
+    else if(w_t<19){content.innerHTML+="<img src=\"images/test-medium.jpeg\">";}
     else {content.innerHTML+="<img src=\"images/test-high.jpeg\">";}
 }
 
