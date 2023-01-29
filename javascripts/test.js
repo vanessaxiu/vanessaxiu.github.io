10a2606cb736b99c6e63a4652ca2665a886498d3
vanessaxiu <77381325+vanessaxiu@users.noreply.github.com>
2023-01-29 10:30:30 -0500

    update test

--- javascripts/test.js
+++ javascripts/test.js
@@ -1,12 +1,12 @@
 
 var quiz = [
     {
-        "question": "Question01",
-        "choices": ["choice1", "choice2", "choice3", "choice4"],
-        "w_i": [1, 2, 3, 4]
+        "question": "what is the gender you were assigned at birth",
+        "choices": ["male", "female"],
+        "w_i": [48.86, 21.14]
     }, {
-        "question": "Question02",
-        "choices": ["choice1", "choice2", "choice3", "choice4"],
+        "question": "in the last 12 months, have you had sex with a male",
+        "choices": ["yes", "choice2", "choice3", "choice4"],
         "w_i": [1, 2, 3, 4]
     }, {
         "question": "Question03",
@@ -22,13 +22,11 @@ var quiz = [
         "w_i": [1, 2, 3, 4]
     }
 ];
-// define elements
 
-// init vars
 var i_q = 0,
     w_t = 0;
 
-function $(id) { // shortcut for document.getElementById
+function $(id) {
     return document.getElementById(id);
 }
 
@@ -40,8 +38,6 @@ var content = $("content"),
 function ask() {
     var choices = quiz[i_q].choices,
         choicesHtml = "";
-
-    // loop through choices, and create radio buttons
     for (var i = 0; i < choices.length; i++) {
         choicesHtml += "<input type='radio' name='quiz" + i_q +
         "' id='choice" + (i + 1) +
@@ -49,24 +45,20 @@ function ask() {
         " <label for='choice" + (i + 1) + "'>" + choices[i] + "</label><br>";
     }
 
-    // load the question
     questionContainer.textContent = "Q" + (i_q + 1) + ". " +
     quiz[i_q].question;
 
-    // load the choices
     choicesContainer.innerHTML = choicesHtml;
 }
 
 function check() {
-    // determine which radio button they clicked
     var radios = document.getElementsByName("quiz" + i_q);
     for (var i = 0; i < radios.length; i++) {
-        if (radios[i].checked) { // if this radio button is checked
+        if (radios[i].checked) {
             w_t = w_t + quiz[i_q].w_i[i];
         }
     }
 
-    // if we're not on last question, increase question number
     if (i_q < quiz.length - 1) {
         i_q++;
         ask();
@@ -78,9 +70,11 @@ function check() {
 
 function grade() {
     document.getElementById('submitBtn').disabled="true";
+    content.innerHTML = "<h2>Thank you for completing the test!</h2>" + "<h2>Below are your risk scale and suggested treatment options:</h2>"
     if (w_t <10){
-    content.innerHTML="<img src=\"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/apples-at-farmers-market-royalty-free-image-1627321463.jpg?crop=1.00xw:0.631xh;0.00160xw,0.206xh&resize=980:*\">";}
-    else{content.innerHTML="<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/800px-Banana-Single.jpg\">";}
+    content.innerHTML += "<img src=\"images/test-low.jpeg\">";}
+    else if(w_t<20){content.innerHTML+="<img src=\"images/test-medium.jpeg\">";}
+    else {content.innerHTML+="<img src=\"images/test-high.jpeg\">";}
 }
 
 
@@ -89,7 +83,7 @@ submitBtn.addEventListener("click", check, false);
 (function(count){
     $("#submitBtn").click(function(){
         if(++count === 2) this.style.display = 'none';
-        return false; // returning false prevents form submission
+        return false;
     });
 })(0);
 
